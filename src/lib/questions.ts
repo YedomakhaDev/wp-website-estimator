73c4cd4761d4c32a99d535d12adf082daf3aef66
Yan Yedomakha <yedomakha.dev@gmail.com>
2026-09-22 20:37:33 +0300

    feat: add first question (site type)

--- src/lib/questions.ts
+++ src/lib/questions.ts
@@ -1,3 +1,15 @@
 import { Question } from "./types";
 
-export const questions: Question[] = [];
+export const questions: Question[] = [
+    {
+        id: "site_type",
+        label: "What type of website do you need?",
+        type: "single-choice",
+        options: [
+            { value: "brochure", label: "Brochure / business site" },
+            { value: "blog", label: "Blog" },
+            { value: "store", label: "Online store" },
+            { value: "custom_app", label: "Custom web application" },
+        ],
+    },
+];
